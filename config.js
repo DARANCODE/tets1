e6f7afe25dde320234e998790099628f6c2d5f5c
DARANCODE <99070344+DARANCODE@users.noreply.github.com>
2022-09-23 17:08:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "lOVE U",
+    introTitle: 'Gửi cậu',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu mình <3',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Yêu mình rùi  mai rủ mình đi căn tin  đi :33:',
+    btnAccept: 'Okii lunn :3',
+    
+    messLink: 'https://www.facebook.com/daran06' //link mess của các bạn. VD: https://www.facebook.com/messages/t/1000000000004188333536
+}
